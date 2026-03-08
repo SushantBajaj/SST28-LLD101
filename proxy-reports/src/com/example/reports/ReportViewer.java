3fe5f3be8b524d6fdb726bf33b8ed6585ee87d3a
SushantBajaj <sushant1108bajaj@gmail.com>
2026-03-09 03:15:57 +0530

    refactored proxy-reports

--- proxy-reports/src/com/example/reports/ReportViewer.java
+++ proxy-reports/src/com/example/reports/ReportViewer.java
@@ -5,9 +5,10 @@ package com.example.reports;
  * - Viewer depends directly on concrete ReportFile
  * - No Proxy involved
  */
+
 public class ReportViewer {
 
-    public void open(ReportFile report, User user) {
+    public void open(Report report, User user) {
         report.display(user);
     }
 }
